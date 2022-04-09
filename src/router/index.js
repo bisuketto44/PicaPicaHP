ff0fd16b853fb946cb3c36dcd094385e21735b36
bisuketto44 <utyua0229@gmail.com>
2022-04-09 16:12:28 +0900

    sitemap

--- src/router/index.js
+++ src/router/index.js
@@ -3,45 +3,49 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+export const routes = [{
+    path: '/',
+    name: 'Home', // URL 
+    component: () => import( /* webpackChunkName: "about" */ '../views/Home.vue')
+  },
+  {
+    path: '/store1',
+    name: 'Store1', // URL 
+    component: () => import( /* webpackChunkName: "about" */ '../views/Store1.vue')
+  },
+  {
+    path: '/store1_menu',
+    name: 'Store1_Menu', // URL 
+    component: () => import( /* webpackChunkName: "about" */ '../views/Store1_Menu.vue')
+  },
+  {
+    path: '/store2',
+    name: 'Store2', // URL 
+    component: () => import( /* webpackChunkName: "about" */ '../views/Store2.vue')
+  },
+  {
+    path: '/store2_menu',
+    name: 'Store2_Menu', // URL 
+    component: () => import( /* webpackChunkName: "about" */ '../views/Store2_Menu.vue')
+  },
+  {
+    path: '/store2_info',
+    name: 'Store2_Info', // URL 
+    component: () => import( /* webpackChunkName: "about" */ '../views/Store2_StoreInfo.vue')
+  },
+  {
+    path: '/recruit',
+    name: 'Recruit', // URL 
+    component: () => import( /* webpackChunkName: "about" */ '../views/Recruit.vue')
+  },
+]
+
 export default new Router({
 
+  mode: 'history',
+  base: '/PicaPicaHP/',
   // 遷移を記述します
-  routes: [{
-      path: '/',
-      name: 'Home', // URL 
-      component: () => import( /* webpackChunkName: "about" */ '../views/Home.vue')
-    },
-    {
-      path: '/store1',
-      name: 'Store1', // URL 
-      component: () => import( /* webpackChunkName: "about" */ '../views/Store1.vue')
-    },
-    {
-      path: '/store1_menu',
-      name: 'Store1_Menu', // URL 
-      component: () => import( /* webpackChunkName: "about" */ '../views/Store1_Menu.vue')
-    },
-    {
-      path: '/store2',
-      name: 'Store2', // URL 
-      component: () => import( /* webpackChunkName: "about" */ '../views/Store2.vue')
-    },
-    {
-      path: '/store2_menu',
-      name: 'Store2_Menu', // URL 
-      component: () => import( /* webpackChunkName: "about" */ '../views/Store2_Menu.vue')
-    },
-    {
-      path: '/store2_info',
-      name: 'Store2_Info', // URL 
-      component: () => import( /* webpackChunkName: "about" */ '../views/Store2_StoreInfo.vue')
-    },
-    {
-      path: '/recruit',
-      name: 'Recruit', // URL 
-      component: () => import( /* webpackChunkName: "about" */ '../views/Recruit.vue')
-    },
-  ],
+  routes,
 
   // デフォだと同じコンポーネント内では同じスクロール位置を返すので、通常通りの動きになる処理を加えておく
   scrollBehavior(to, from, savePosition) {
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
+require = require('esm')(module);
+const {
+  routes
+} = require('./src/router');
+
+var sitemapRoutes = routes.map(x => {
+  x.path = `/PicaPicaHP${x.path}`;
+  return x;
+})
+
 module.exports = {
-    outputDir: 'docs',
-    assetsDir: './',
-    publicPath: './'
+  outputDir: 'docs',
+  assetsDir: './',
+  publicPath: './',
+  pluginOptions: {
+    sitemap: {
+      baseURL: 'https://bisuketto44.github.io',
+      routes: sitemapRoutes,
+    }
   }
+}
