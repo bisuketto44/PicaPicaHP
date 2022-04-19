1c6b912465ce14bd9d35501164f758d3e8b5f849
bisuketto44 <utyua0229@gmail.com>
2022-04-20 05:51:45 +0900

    fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,89 +1,86 @@
 <template>
-  <div class="menu">
+  <div class="Home">
     <div class="PicaPicaTop">
       <h1>社会福祉法人 ゆるり福祉会<span>Pica Pica</span></h1>
     </div>
-    <div class="contentsWarpper">
-      <div class="Incontents">
-        <div class="HomesTOP">
-          <h2>●ゆるり福祉会とは</h2>
-          <div class="HomeTopText2">
-            <p>
-              2006年4月、自立支援法が施行、10月より本格的に実施され、<br />これまでの障がい者福祉は大きく変わることとなりました。<br />
-              社会福祉法人ゆるり福祉会では、仕事及び日々の生活を地域社会で暮らせるよう、<br />毎日通所できるB型を選択し認可されています。<br />
-              スタッフ、メンバー一丸となって「ともに」仕事をしています。
-            </p>
-          </div>
-        </div>
 
-        <div class="Homes">
-          <h2>●概要</h2>
-          <div class="TOPTEXT">
-            <div class="over">
-              <p>○照合 / 社会福祉法人ゆるり福祉会</p>
-              <p>○創業 / 1998 年 6月</p>
-              <p>○設立 / 2006 年 4月</p>
-              <p>○代表者 / 井藤 陽介</p>
-              <p>○所在地 /</p>
-              <ul>
-                <li>〒581-0043</li>
-                <li>大阪府八尾市空港 1-146-2 大阪府中部広防災域防災拠点前</li>
-                <li>TEL 072-997-7475</li>
-                <li>FAX 072-993-1847</li>
-                <li>pica2@eco.ocn.ne.jp</li>
-              </ul>
-              <p>○事前認可 / 指定障がい者福祉サービス事業者</p>
-              <p>○障がい福祉サービスの種類 / 就労継続支援B型(35名)</p>
-              <p>
-                ○事業内容 /
-                パン・クッキー・ランチ(パスタ料理他)・軽食・喫茶・宅配弁当<br />陶芸・さをり・河内木綿・かみすき和紙・縫製・野菜づくりなど
-              </p>
-            </div>
-          </div>
-        </div>
+    <div class="HomesTOP">
+      <h2>●ゆるり福祉会とは</h2>
+      <div class="HomeTopText2">
+        <p>
+          2006年4月、自立支援法が施行、10月より本格的に実施され、<br />これまでの障がい者福祉は大きく変わることとなりました。<br />
+          社会福祉法人ゆるり福祉会では、仕事及び日々の生活を地域社会で暮らせるよう、<br />毎日通所できるB型を選択し認可されています。<br />
+          スタッフ、メンバー一丸となって「ともに」仕事をしています。
+        </p>
+      </div>
+    </div>
 
-        <div class="Homes">
-          <h2>●沿革</h2>
-          <div class="TOPTEXT">
-            <div class="over2">
-              <p>1988年 6月 / 東部大阪障がい者労働センター 設立</p>
-              <p>1991年 6月 / 無認可作業所 にっこり作業所 設立</p>
-              <p>1995年 4月 / 無認可作業所 PicaPica 設立</p>
-              <p>2006年 4月 / (福祉) ゆるり福祉会 設立</p>
-              <p>2008年 4月 / (福祉) ゆるり福祉会 Pica Pica 空港店 開所</p>
-              <p>2013年 12月 / (福祉) ゆるり福祉会 Pica Pica 市役所店 開所</p>
-            </div>
-          </div>
+    <div class="Homes">
+      <h2>●概要</h2>
+      <div class="TOPTEXT">
+        <div class="over">
+          <p>○照合 / 社会福祉法人ゆるり福祉会</p>
+          <p>○創業 / 1998 年 6月</p>
+          <p>○設立 / 2006 年 4月</p>
+          <p>○代表者 / 井藤 陽介</p>
+          <p>○所在地 /</p>
+          <ul>
+            <li>〒581-0043</li>
+            <li>大阪府八尾市空港 1-146-2 大阪府中部広防災域防災拠点前</li>
+            <li>TEL 072-997-7475</li>
+            <li>FAX 072-993-1847</li>
+            <li>pica2@eco.ocn.ne.jp</li>
+          </ul>
+          <p>○事前認可 / 指定障がい者福祉サービス事業者</p>
+          <p>○障がい福祉サービスの種類 / 就労継続支援B型(35名)</p>
+          <p>
+            ○事業内容 /
+            パン・クッキー・ランチ(パスタ料理他)・軽食・喫茶・宅配弁当<br />陶芸・さをり・河内木綿・かみすき和紙・縫製・野菜づくりなど
+          </p>
         </div>
+      </div>
+    </div>
 
-        <div class="Homes">
-          <h2>●基本理念</h2>
-          <div class="TOPTEXT">
-            <div class="over2">
-              <p>
-                ○利用者の基本的人権を保障および尊重し、障がいの有無に関わらず、<br />
-                &nbsp;&nbsp;全ての人々が平等に生活する社会=ノーマライゼーション社会の<br />&nbsp;&nbsp;実現を目指します。
-              </p>
-              <p>
-                ○利用者一人ひとりの人格・個性を尊重した就労・社会参加・自立支援を実践し、<br />&nbsp;&nbsp;地域社会に開かれた施設作りを目指します。
-              </p>
-            </div>
-          </div>
+    <div class="Homes">
+      <h2>●沿革</h2>
+      <div class="TOPTEXT">
+        <div class="over2">
+          <p>1988年 6月 / 東部大阪障がい者労働センター 設立</p>
+          <p>1991年 6月 / 無認可作業所 にっこり作業所 設立</p>
+          <p>1995年 4月 / 無認可作業所 PicaPica 設立</p>
+          <p>2006年 4月 / (福祉) ゆるり福祉会 設立</p>
+          <p>2008年 4月 / (福祉) ゆるり福祉会 Pica Pica 空港店 開所</p>
+          <p>2013年 12月 / (福祉) ゆるり福祉会 Pica Pica 市役所店 開所</p>
         </div>
+      </div>
+    </div>
 
-        <div class="Homes">
-          <h2>●財務諸表 / 定款</h2>
-          <div class="TOPTEXTFoot">
-            <div class="TOPTEXT">
-              <div class="over2">
-                <a
-                  href="https://www.wam.go.jp/wamnet/zaihyoukaiji/pub/PUB0201000E00.do?_FORMID=PUB0219000&vo_headVO_corporationId=1627106320"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  >掲載情報はこちら</a
-                >
-              </div>
-            </div>
+    <div class="Homes">
+      <h2>●基本理念</h2>
+      <div class="TOPTEXT">
+        <div class="over2">
+          <p>
+            ○利用者の基本的人権を保障および尊重し、障がいの有無に関わらず、<br />
+            &nbsp;&nbsp;全ての人々が平等に生活する社会=ノーマライゼーション社会の<br />&nbsp;&nbsp;実現を目指します。
+          </p>
+          <p>
+            ○利用者一人ひとりの人格・個性を尊重した就労・社会参加・自立支援を実践し、<br />&nbsp;&nbsp;地域社会に開かれた施設作りを目指します。
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="Homes">
+      <h2>●財務諸表 / 定款</h2>
+      <div class="TOPTEXTFoot">
+        <div class="TOPTEXT">
+          <div class="over2">
+            <a
+              href="https://www.wam.go.jp/wamnet/zaihyoukaiji/pub/PUB0201000E00.do?_FORMID=PUB0219000&vo_headVO_corporationId=1627106320"
+              target="_blank"
+              rel="noopener noreferrer"
+              >掲載情報はこちら</a
+            >
           </div>
         </div>
       </div>
@@ -243,7 +240,7 @@ span {
   }
 
   .over2 a {
-    font-size: small;
+    font-size: large;
     letter-spacing: 1px;
   }
 
